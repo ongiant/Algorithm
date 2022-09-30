788053c32b4f803747aae178266df0438912191a
Zhong Shiang <zhongshiang@gmail.com>
2022-10-01 00:16:44 +0800

    inclusion-exclusion principle

---
+++ src/acwing/math/inclusion_exclusion_principle/DivisibleNumbers_890.java
@@ -0,0 +1,42 @@
+package acwing.math.inclusion_exclusion_principle;
+
+import java.util.Scanner;
+
+public class DivisibleNumbers_890 {
+
+    static final int N = 20;
+    static int n, m;
+
+    static int[] p;
+
+    static {
+        p = new int[N];
+    }
+
+    public static void main(String[] args){
+        Scanner sin = new Scanner(System.in);
+
+        n = sin.nextInt();
+        m = sin.nextInt();
+
+        for(int i = 0; i < m; i ++) p[i] = sin.nextInt();
+
+        int ans = 0;
+        for(int i = 1; i < 1 << m; i ++){
+            int cnt = 0;
+            long t = 1;
+            for(int j = 0; j < m; j ++){
+                if((i >> j & 1) == 1){
+                    cnt ++;
+                    t *= p[j];
+                    if(t > n) break;
+                }
+            }
+            if((cnt & 1) == 1) ans += n / t;
+            else ans -= n / t;
+        }
+
+        System.out.println(ans);
+        sin.close();
+    }
+}
