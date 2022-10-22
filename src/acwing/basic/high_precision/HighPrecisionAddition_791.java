1d41d738d8bf91bd2e900c296e7d1012fe413e92
Zhong Shiang <zhongshiang@gmail.com>
2022-10-22 15:48:42 +0800

    finish big number add

---
+++ src/acwing/basic/high_precision/HighPrecisionAddition_791.java
@@ -0,0 +1,33 @@
+package acwing.basic.high_precision;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class HighPrecisionAddition_791 {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String x = br.readLine(), y = br.readLine();
+        System.out.println(add(x, y));
+
+        br.close();
+    }
+
+    static String add(String x, String y){
+        if(x.length() < y.length()) return add(y, x);
+
+        StringBuilder sbr = new StringBuilder();
+        int t = 0;
+        for(int i = x.length() - 1, j = y.length() - 1; i >= 0; i --, j --){
+            t += x.charAt(i) - '0';
+            if(j >= 0) t += y.charAt(j) - '0';
+            sbr.append(t % 10);
+            t /= 10;
+        }
+
+        if(t > 0) sbr.append(t);
+        return sbr.reverse().toString();
+    }
+}
